0e4440825bd4def107d3fdad69ee77b8070e38a6
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-10-24 09:16:01 -0300

    change main.tsx and add styles for input tag in index.css

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Form } from "./components/Form";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center w-full min-h-screen bg-gray-100">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
