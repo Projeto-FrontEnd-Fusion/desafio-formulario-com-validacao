297d78d43aa6f92e627ff57bd5968c5e0d1ebdae
EriveltonMGit <eriveltonprojetos.git@gmail.com>
2025-01-06 18:43:00 -0300

    chore: removendo Aos

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,12 @@ import store from './Redux/store.tsx';
 import App from './App.jsx';
 import './index.css';
 import "antd/dist/reset.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-AOS.init({
-  duration: 1000, // Duração da animação
-  offset: 200, // Distância de ativação
-});
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+// AOS.init({
+//   duration: 1000, // Duração da animação
+//   offset: 200, // Distância de ativação
+// });
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}> {/* Envolve o App com o Provider */}
